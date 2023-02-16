f3701b30919f4520ab9a8c3b5c31d25a9e6d0927
dlctechs <dlctechs1@gmail.com>
2023-02-16 03:14:45 -0800

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,5 @@
-es (11 sloc)  237 Bytes
-
 #include <stdio.h>
-
+  
 /**
  * main - Entry point
  * Description: prints "\"Programming is like building a multilingual puzzle"
@@ -12,4 +10,4 @@ int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-}
+}		
