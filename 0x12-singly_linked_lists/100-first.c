a9a8079011d23cb9ba61ea285723cb690d0dead5
dlctechs <dlctechs1@gmail.com>
2023-04-05 08:20:21 -0700

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,19 +1,13 @@
-#include "lists.h"
-#include <stdlib.h>
+#include <stdio.h>
+
+void first(void) __attribute__ ((constructor));
 
 /**
- * free_list - Frees a list_t list.
- * @head: A pointer to the list_t list.
+ * first - prints a sentence before the main
+ * function is executed
  */
-void free_list(list_t *head)
+void first(void)
 {
-	list_t *tmp;
-
-	while (head)
-	{
-		tmp = head->next;
-		free(head->str);
-		free(head);
-		head = tmp;
-	}
+ printf("You're beat! and yet, you must allow,\n");
+ printf("I bore my house upon my back!\n");
 }
